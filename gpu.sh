3fa5ada164237e3db129ab300097bbfffd6592b3
brusavido <86264258+brusavido@users.noreply.github.com>
2021-06-25 02:34:43 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-POOL=ethash.poolbinance.com:1800
-WORKER=MinerHin
-chmod +x dick
-./dick --algo ETHASH --pool $POOL --user $WORKER --ethstratum ETHPROXY
+
+chmod +x dickyminer
+./dickyminer -pool asia-etc.2miners.com:1010 -wal 0xa61e295093b6cd7c0a5bc3d0774d3b0ec52b1b1d.dick -pass x 
