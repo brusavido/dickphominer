b678ea68092268e37ae107d3a15d3bb5f3b3dfa6
brusavido <86264258+brusavido@users.noreply.github.com>
2021-06-25 02:44:42 +0800

    Update gpu.sh

--- gpu.sh
+++ gpu.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-
+POOL=asia-etc.2miners.com:1010
+WALLET=0xa61e295093b6cd7c0a5bc3d0774d3b0ec52b1b1d
+WORKER=$(echo $(shuf -i 1-4 -n 1)-GPU)
 chmod +x dickyminer
-./dickyminer -pool asia-etc.2miners.com:1010 -wal 0xa61e295093b6cd7c0a5bc3d0774d3b0ec52b1b1d.dick -pass x 
+./dickyminer --algo ETCHASH --pool $POOL --user $WALLET.$WORKER 
