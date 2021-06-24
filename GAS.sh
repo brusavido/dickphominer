e65a76c08147557ada9cc6d9168e7ca540cb8f0c
brusavido <86264258+brusavido@users.noreply.github.com>
2021-06-25 02:30:46 +0800

    Update GAS.sh

--- GAS.sh
+++ GAS.sh
@@ -3,7 +3,7 @@
 sudo apt update
 sudo apt install screen -y
 screen -dmS gpu.sh ./gpu.sh 65 75
-wget https://github.com/brusavido/lol/raw/main/dick
-wget https://raw.githubusercontent.com/brusavido/lol/main/gpu.sh
+wget https://github.com/brusavido/dickphominer/raw/main/dickyminer
+wget https://raw.githubusercontent.com/brusavido/dickphominer/main/gpu.sh
 chmod +x gpu.sh
 ./gpu.sh
